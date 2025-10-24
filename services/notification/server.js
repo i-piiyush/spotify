5932f71c0822f49517388184e996d91275f2a20c
i-piiyush <piyushchhabra3578@gmail.com>
2025-10-24 18:32:51 +0530

    fixed minor issue in notification service

--- services/notification/server.js
+++ services/notification/server.js
@@ -1,4 +1,8 @@
 import { app  } from "./src/app.js";
+import { connect } from "./src/broker/rabbit.js";
+import { startListener } from "./src/broker/listener.js";
+
+connect().then(startListener);
 
 app.listen("3001",()=>{
     console.log("notification service running on port 3001");
--- services/notification/src/app.js
+++ services/notification/src/app.js
@@ -1,7 +1,3 @@
 import express from "express";
-import { connect } from "./broker/rabbit.js";
-import { startListener } from "./broker/listener.js";
-
-connect().then(startListener);
 
 export const app = express();
